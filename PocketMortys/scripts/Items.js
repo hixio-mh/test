b0b4e06e706903b3e5fb54a49a4bccd26fb82861
VLADOS776 <kurtukovvlad@gmail.com>
2016-03-07 17:07:27 +0300

    Pocket Mortys

---
+++ PocketMortys/scripts/Items.js
@@ -0,0 +1,405 @@
+window.Mortys = window.Mortys || {};
+Mortys = {
+	"Items" : [{
+			'id' : 1,
+			'name' : 'Morty Manipulator Chip',
+			'rarity' : 30,
+			'img' : 'MortyManipulatorChip.png',
+			'cost' : 500,
+			'craft' : [
+				17,
+				23,
+				27,
+			]
+		}, {
+			'id' : 2,
+			'name' : 'Serum',
+			'rarity' : 100,
+			'img' : 'Serum.png',
+			'cost' : 250,
+			'craft' : [
+				16,
+				19,
+			]
+		}, {
+			'id' : 3,
+			'name' : 'Great Serum',
+			'rarity' : 75,
+			'img' : 'GreatSerum.png',
+			'cost' : 500,
+			'craft' : [
+				16,
+				20,
+			]
+		}, {
+			'id' : 4,
+			'name' : 'Sensational Serum',
+			'rarity' : 50,
+			'img' : 'SensationalSerum.png',
+			'cost' : 1200,
+			'craft' : [
+				17,
+				2,
+			]
+		}, {
+			'id' : 5,
+			'name' : 'Pure Serum',
+			'rarity' : 25,
+			'img' : 'PureSerum.png',
+			'cost' : 1500,
+			'craft' : [
+				17,
+				3,
+			]
+		}, {
+			'id' : 6,
+			'name' : 'Plutonic Rock',
+			'rarity' : 80,
+			'img' : 'PlutonicRock.png',
+			'cost' : 200,
+			'craft' : [
+				16,
+				21,
+			]
+		}, {
+			'id' : 7,
+			'name' : 'Pure Plutonic Rock',
+			'rarity' : 50,
+			'img' : 'PurePlutonicRock.png',
+			'cost' : 800,
+			'craft' : [
+				16,
+				22,
+			]
+		}, {
+			'id' : 8,
+			'name' : 'Halzinger',
+			'rarity' : 50,
+			'img' : 'Halzinger.png',
+			'cost' : 650,
+			'craft' : [
+				16,
+				19,
+				21,
+			]
+		}, {
+			'id' : 9,
+			'name' : 'Pure Halzinger',
+			'rarity' : 30,
+			'img' : 'PureHalzinger.png',
+			'cost' : 1800,
+			'craft' : [
+				16,
+				20,
+				22,
+			]
+		}, {
+			'id' : 10,
+			'name' : 'Mr. Meeseeks Box',
+			'rarity' : 10,
+			'img' : 'MrMeeseeksBox.png',
+			'cost' : '?',
+			'craft' : [
+			]
+		}, {
+			'id' : 11,
+			'name' : 'Attack Mega Seed',
+			'rarity' : 15,
+			'img' : 'AttackMegaSeed.png',
+			'cost' : 1000,
+			'craft' : [
+			]
+		}, {
+			'id' : 12,
+			'name' : 'Defense Mega Seed',
+			'rarity' : 15,
+			'img' : 'DefenseMegaSeed.png',
+			'cost' : 1000,
+			'craft' : [
+			]
+		}, {
+			'id' : 13,
+			'name' : 'Speed Mega Seed',
+			'rarity' : 15,
+			'img' : 'SpeedMegaSeed.png',
+			'cost' : 1000,
+			'craft' : [
+			]
+		}, {
+			'id' : 14,
+			'name' : 'Level Up Mega Seed',
+			'rarity' : 15,
+			'img' : 'LevelUpMegaSeed.png',
+			'cost' : 2500,
+			'craft' : [
+				11,
+				12,
+				13,
+			]
+		}, {
+			'id' : 15,
+			'name' : 'Blips and Chitz Coupon',
+			'rarity' : 10,
+			'img' : 'BlipsandChitzCoupon.png',
+			'cost' : '?',
+			'craft' : [
+			]
+		}, {
+			'id' : 16,
+			'name' : 'Battery',
+			'rarity' : 100,
+			'img' : 'Battery.png',
+			'cost' : 200,
+			'craft' : [
+				27,
+				28,
+				19,
+			]
+		}, {
+			'id' : 17,
+			'name' : 'Supercharged Battery',
+			'rarity' : 100,
+			'img' : 'SuperchargedBattery.png',
+			'cost' : '?',
+			'craft' : [
+				16,
+				29,
+			]
+		}, {
+			'id' : 18,
+			'name' : 'Microverse Battery',
+			'rarity' : 100,
+			'img' : 'MicroverseBattery.png',
+			'cost' : '?',
+			'craft' : [
+				17,
+				24,
+				26,
+			]
+		}, {
+			'id' : 19,
+			'name' : 'Fleeb',
+			'rarity' : 100,
+			'img' : 'Fleeb.png',
+			'cost' : '?',
+			'craft' : [
+			]
+		}, {
+			'id' : 20,
+			'name' : 'Purified Fleeb',
+			'rarity' : 100,
+			'img' : 'PurifiedFleeb.png',
+			'cost' : '?',
+			'craft' : [
+				19,
+				29,
+			]
+		}, {
+			'id' : 21,
+			'name' : 'Bacteria Cell',
+			'rarity' : 100,
+			'img' : 'BacteriaCell.png',
+			'cost' : '?',
+			'craft' : [
+			]
+		}, {
+			'id' : 22,
+			'name' : 'Mutant Bacteria Cell',
+			'rarity' : 100,
+			'img' : 'MutantBacteriaCell.png',
+			'cost' : '?',
+			'craft' : [
+				21,
+				29,
+			]
+		}, {
+			'id' : 23,
+			'name' : 'Circuit Board',
+			'rarity' : 100,
+			'img' : 'CircuitBoard.png',
+			'cost' : '?',
+			'craft' : [
+			]
+		}, {
+			'id' : 24,
+			'name' : 'Motherboard',
+			'rarity' : 100,
+			'img' : 'Motherboard.png',
+			'cost' : '?',
+			'craft' : [
+				16,
+				23,
+				28,
+			]
+		}, {
+			'id' : 25,
+			'name' : 'Dark Energy Ball',
+			'rarity' : 100,
+			'img' : 'DarkEnergyBall.png',
+			'cost' : '?',
+			'craft' : [
+				19,
+				21,
+			]
+		}, {
+			'id' : 26,
+			'name' : 'Dark Matter Ball',
+			'rarity' : 100,
+			'img' : 'DarkMatterBall.png',
+			'cost' : '?',
+			'craft' : [
+				25,
+				29,
+			]
+		}, {
+			'id' : 27,
+			'name' : 'Tin Can',
+			'rarity' : 100,
+			'img' : 'TinCan.png',
+			'cost' : '?',
+			'craft' : [
+			]
+		}, {
+			'id' : 28,
+			'name' : 'Cable',
+			'rarity' : 100,
+			'img' : 'Cable.png',
+			'cost' : '?',
+			'craft' : [
+			]
+		}, {
+			'id' : 29,
+			'name' : 'Turbulent Juice Tube',
+			'rarity' : 100,
+			'img' : 'TurbulentJuiceTube.png',
+			'cost' : '?',
+			'craft' : [
+			]
+		}, {
+			'id' : 30,
+			'name' : 'Dog Collar',
+			'rarity' : 100,
+			'img' : 'DogCollar.png',
+			'cost' : '?',
+			'craft' : [
+				28,
+				27,
+			]
+		}, {
+			'id' : 31,
+			'name' : 'Love Potion',
+			'rarity' : 100,
+			'img' : 'LovePotion.png',
+			'cost' : '?',
+			'craft' : [
+				20,
+				22,
+			]
+		}, {
+			'id' : 32,
+			'name' : 'Robot',
+			'rarity' : 100,
+			'img' : 'Robot.png',
+			'cost' : '?',
+			'craft' : [
+				17,
+				24,
+			]
+		}, {
+			'id' : 33,
+			'name' : 'Butter Robot',
+			'rarity' : 100,
+			'img' : 'ButterRobot.png',
+			'cost' : '?',
+			'craft' : [
+				17,
+				24,
+				32,
+			]
+		}, {
+			'id' : 34,
+			'name' : 'Time Crystal',
+			'rarity' : 100,
+			'img' : 'TimeCrystal.png',
+			'cost' : '?',
+			'craft' : [
+				20,
+				25,
+			]
+		}, {
+			'id' : 35,
+			'name' : 'Time Stabilizing Collar',
+			'rarity' : 100,
+			'img' : 'TimeStabilizingCollar.png',
+			'cost' : '?',
+			'craft' : [
+				34,
+				30,
+			]
+		}, {
+			'id' : 36,
+			'name' : 'Gwendolyn Doll',
+			'rarity' : 100,
+			'img' : 'GwendolynDoll.png',
+			'cost' : '?',
+			'craft' : [
+				32,
+				31,
+			]
+		}, {
+			'id' : 37,
+			'name' : 'Interdimensional Cable Box',
+			'rarity' : 100,
+			'img' : 'InterdimensionalCableBox.png',
+			'cost' : '?',
+			'craft' : [
+				17,
+				24,
+				34,
+			]
+		}, {
+			'id' : 38,
+			'name' : 'Neutrino Bomb',
+			'rarity' : 100,
+			'img' : 'NeutrinoBomb.png',
+			'cost' : '?',
+			'craft' : [
+				17,
+				24,
+				25,
+			]
+		}, {
+			'id' : 39,
+			'name' : 'Interdimensional Goggles',
+			'rarity' : 100,
+			'img' : 'InterdimensionalGoggles.png',
+			'cost' : '?',
+			'craft' : [
+				17,
+				34,
+				27,
+			]
+		}, {
+			'id' : 40,
+			'name' : 'IQ Enhancing Helmet',
+			'rarity' : 100,
+			'img' : 'IQEnhancingHelmet.png',
+			'cost' : '?',
+			'craft' : [
+				17,
+				24,
+				29,
+			]
+		}, {
+			'id' : 41,
+			'name' : 'Roy VR Headset',
+			'rarity' : 100,
+			'img' : 'RoyVRHeadset.png',
+			'cost' : '?',
+			'craft' : [
+				39,
+				40,
+			]
+		}
+	]
+}
