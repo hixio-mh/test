83cc3ab082913104171ac0dafdbfe060afad888f
VLADOS776 <kurtukovvlad@gmail.com>
2016-05-15 21:11:08 +0300

    Scrap Mechanic Новый дизайн механизмов

---
+++ ScrapMechanic/scripts/mechanisms.js
@@ -0,0 +1,326 @@
+﻿var mechanisms = [{
+		"name" : {
+			"ENG" : "Contraption Builder Basics!",
+			"RUS" : "Основы строительства"
+		},
+		"youtube" : "https://www.youtube.com/embed/isnVgJ6cGLs"
+	}, {
+		"name" : {
+			"ENG" : "Transforming House-Car!",
+			"RUS" : "Дом-машина"
+		},
+		"youtube" : "https://www.youtube.com/embed/7MlXO8DHTcc"
+	}, {
+		"name" : {
+			"ENG" : "Sliding Doors & Elevators!",
+			"RUS" : "Сдвигающиеся дверь и лифт"
+		},
+		"youtube" : "https://www.youtube.com/embed/a8QBtQmrzXI"
+	}, {
+		"name" : {
+			"ENG" : "The Spider Walker",
+			"RUS" : "Паук"
+		},
+		"youtube" : "https://www.youtube.com/embed/oX4sg6ZEcY8"
+	}, {
+		"name" : {
+			"ENG" : "The Spider Walker 2.0",
+			"RUS" : "Паук 2.0"
+		},
+		"youtube" : "https://www.youtube.com/embed/oX4sg6ZEcY8"
+	}, {
+		"name" : {
+			"ENG" : "Designing A No-Flip Car",
+			"RUS" : "Не переворачивающаяся машина"
+		},
+		"youtube" : "https://www.youtube.com/embed/ioZhtortLro"
+	}, {
+		"name" : {
+			"ENG" : "Semi-Truck",
+			"RUS" : "Грузовик с прицепом"
+		},
+		"youtube" : "https://www.youtube.com/embed/XGOr_XPnd0M"
+	}, {
+		"name" : {
+			"ENG" : "Axolot Car",
+			"RUS" : "Машина из трейлера (Axolot Car)"
+		},
+		"youtube" : "https://www.youtube.com/embed/2fBiyqCxdHI"
+	}, {
+		"name" : {
+			"ENG" : "The Footmobile",
+			"RUS" : "Ходячая машина"
+		},
+		"youtube" : "https://www.youtube.com/embed/oQY5_2qcNoo"
+	}, {
+		"name" : {
+			"ENG" : "Multiplayer Mini-Games",
+			"RUS" : "Мини-игры в мультеплеере"
+		},
+		"youtube" : "https://www.youtube.com/embed/mVl3N0wXVGE"
+	}, {
+		"name" : {
+			"ENG" : "Unstoppable All Terrain Vehicle",
+			"RUS" : "Переворачивающийся вездеход"
+		},
+		"youtube" : "https://www.youtube.com/embed/JcAbpUBHgBQ"
+	}, {
+		"name" : {
+			"ENG" : "Chain Hoist / Crane",
+			"RUS" : "Подъемный кран"
+		},
+		"youtube" : "https://www.youtube.com/embed/9nFaiSfwKsI"
+	}, {
+		"name" : {
+			"ENG" : "Gondola",
+			"RUS" : "Канатная дорога"
+		},
+		"youtube" : "https://www.youtube.com/embed/pYB-urMqQNY"
+	}, {
+		"name" : {
+			"ENG" : "Escalator",
+			"RUS" : "Экскалатор"
+		},
+		"youtube" : "https://www.youtube.com/embed/Ziitts38Qs0"
+	}, {
+		"name" : {
+			"ENG" : "Vehicle Suspension",
+			"RUS" : "Автомобильная подвеска"
+		},
+		"youtube" : "https://www.youtube.com/embed/8VKJjSWnxnQ"
+	}, {
+		"name" : {
+			"ENG" : "Rail System",
+			"RUS" : "Железнодорожная система"
+		},
+		"youtube" : "https://www.youtube.com/embed/iGAXAilYbtQ"
+	}, {
+		"name" : {
+			"ENG" : "Spiral Staircase",
+			"RUS" : "Спиральная лестница"
+		},
+		"youtube" : "https://www.youtube.com/embed/31ncq_nyKsM"
+	}, {
+		"name" : {
+			"ENG" : "Star Trek Door",
+			"RUS" : "Дверь как в Star Trek"
+		},
+		"youtube" : "https://www.youtube.com/embed/hg63sp-LLqk"
+	}, {
+		"name" : {
+			"ENG" : "Horizontal Elevator",
+			"RUS" : "Горизонтальный лифт"
+		},
+		"youtube" : "https://www.youtube.com/embed/Z9jjIe9vdRo"
+	}, {
+		"name" : {
+			"ENG" : "Forklift Truck",
+			"RUS" : "Погрузчик"
+		},
+		"youtube" : "https://www.youtube.com/embed/59eZzA4JWWU"
+	}, {
+		"name" : {
+			"ENG" : "Scissor Lift and Digital Timer/Clock",
+			"RUS" : "Передвижной лифт и таймер/часы"
+		},
+		"youtube" : "https://www.youtube.com/embed/EqQ9vM1xRMU"
+	}, {
+		"name" : {
+			"ENG" : "Open a Door in Style",
+			"RUS" : "Открываем дверь рукой"
+		},
+		"youtube" : "https://www.youtube.com/embed/py8BIgGsXhw"
+	}, {
+		"name" : {
+			"ENG" : "Rocket-Powered Trike",
+			"RUS" : "Трёхколёсный мотоцикл на реактивной тяге"
+		},
+		"youtube" : "https://www.youtube.com/embed/R7qde7CY1BA"
+	}, {
+		"name" : {
+			"ENG" : "Gyroscopic Vehicles",
+			"RUS" : "Гироскопический транспорт"
+		},
+		"youtube" : "https://www.youtube.com/embed/JPXfimGghtk"
+	}, {
+		"name" : {
+			"ENG" : "X WING (Star Wars)",
+			"RUS" : "X WING (из Star Wars)"
+		},
+		"youtube" : "https://www.youtube.com/embed/vBnFDrTN-wo"
+	}, {
+		"name" : {
+			"ENG" : "Land/Air Gliding Sled (NO WHEELS)",
+			"RUS" : "Скользящие сани (без колёс)"
+		},
+		"youtube" : "https://www.youtube.com/embed/MMiLX9zDoxk"
+	}, {
+		"name" : {
+			"ENG" : "A Stable VTOL Rocket Flying Vehicle",
+			"RUS" : "Стабильно летающая платформа"
+		},
+		"youtube" : "https://www.youtube.com/embed/sNdPc0jzsU4"
+	}, {
+		"name" : {
+			"ENG" : "The Car Lift",
+			"RUS" : "Лифт для машины"
+		},
+		"youtube" : "https://www.youtube.com/embed/pRiNAeDC0OY"
+	}, {
+		"name" : {
+			"ENG" : "Transforming Yo-Yo Car",
+			"RUS" : "Машина, трасформирующаяся в Йо-Йо"
+		},
+		"youtube" : "https://www.youtube.com/embed/IvHElcqW4mk"
+	}, {
+		"name" : {
+			"ENG" : "Automated Big Gun",
+			"RUS" : "Автоматическая большая пушка"
+		},
+		"youtube" : "https://www.youtube.com/embed/FN-mgMbaZ24"
+	}, {
+		"name" : {
+			"ENG" : "Dancing Robot",
+			"RUS" : "Танцующий робот"
+		},
+		"youtube" : "https://www.youtube.com/embed/0v8wRxKV2pE"
+	}, {
+		"name" : {
+			"ENG" : "T Flip Flop",
+			"RUS" : ""
+		},
+		"youtube" : "https://www.youtube.com/embed/KEiJga_r-oY"
+	}, {
+		"name" : {
+			"ENG" : "Logic Gates",
+			"RUS" : "Логика"
+		},
+		"youtube" : "https://www.youtube.com/embed/KWqo34oJRSo"
+	}, {
+		"name" : {
+			"ENG" : "Tank",
+			"RUS" : "Танк"
+		},
+		"youtube" : "https://www.youtube.com/embed/VAMVrLtpTBI"
+	}, {
+		"name" : {
+			"ENG" : "Fully Operational Excavator",
+			"RUS" : "Экскаватор"
+		},
+		"youtube" : "https://www.youtube.com/embed/ZssJzsGsweA"
+	}, {
+		"name" : {
+			"ENG" : "The Hand",
+			"RUS" : "Рука"
+		},
+		"youtube" : "https://www.youtube.com/embed/_G-2H_w5xrU"
+	}, {
+		"name" : {
+			"ENG" : "Lowrider, Jip and APC",
+			"RUS" : "Лоурайдер, Джип и БТР"
+		},
+		"youtube" : "https://www.youtube.com/embed/5SyELwPXnVQ"
+	}, {
+		"name" : {
+			"ENG" : "Car/Robot Transformer",
+			"RUS" : "Трансформер"
+		},
+		"youtube" : "https://www.youtube.com/embed/frCuvxzCWUY"
+	}, {
+		"name" : {
+			"ENG" : "Ant",
+			"RUS" : "Шагоход Муравей"
+		},
+		"youtube" : "https://www.youtube.com/embed/tqIM-aJHdik"
+	}, {
+		"name" : {
+			"ENG" : "Russian Jeep",
+			"RUS" : "УАЗ"
+		},
+		"youtube" : "https://www.youtube.com/embed/lR1bBpbK8_Q"
+	}, {
+		"name" : {
+			"ENG" : "Lowrider, monocycle, antigrav",
+			"RUS" : "Лоурайдер, моноцикл, антиграв"
+		},
+		"youtube" : "https://www.youtube.com/embed/SdsGkNYWIGo"
+	}, {
+		"name" : {
+			"ENG" : "Tongue Bridge (Rollup Bridge)",
+			"RUS" : "Мост-язык (Сворачивающийся мост)"
+		},
+		"youtube" : "https://www.youtube.com/embed/sPL6skNgBSc"
+	}, {
+		"name" : {
+			"ENG" : "The Delorean (Transforming/Flying Car)",
+			"RUS" : "Делориан (летающая машина)"
+		},
+		"youtube" : "https://www.youtube.com/embed/IHhzqQzNOWA"
+	}, {
+		"name" : {
+			"ENG" : "HotRod and Batmobile",
+			"RUS" : "HotRod и Бэтмобиль"
+		},
+		"youtube" : "https://www.youtube.com/embed/_Qn5fLp4Yrs"
+	}, {
+		"name" : {
+			"ENG" : "Hummer",
+			"RUS" : "Хаммер"
+		},
+		"youtube" : "https://www.youtube.com/embed/H0MaCm51IIY"
+	}, {
+		"name" : {
+			"ENG" : "The Wasp",
+			"RUS" : "Оса"
+		},
+		"youtube" : "https://www.youtube.com/embed/o0uL6zqp6Z4"
+	}, {
+		"name" : {
+			"ENG" : "The Largest Elevator",
+			"RUS" : "Самый большой лифт"
+		},
+		"youtube" : "https://www.youtube.com/embed/yyJxXK7jV3U"
+	}, {
+		"name" : {
+			"ENG" : "Monowheel, Buggy, Jeep",
+			"RUS" : "Моно колесо, Багги и джип"
+		},
+		"youtube" : "https://www.youtube.com/embed/8YLyrLbM7ic"
+	}, {
+		"name" : {
+			"ENG" : "Cadillac Carbio",
+			"RUS" : "Кадиллак"
+		},
+		"youtube" : "https://www.youtube.com/embed/8OrSTqT8t8I"
+	}, {
+		"name" : {
+			"ENG" : "Epic helicopter, Truck and Buggy",
+			"RUS" : "Эпичный Вертолёт, Тягач и Багги"
+		},
+		"youtube" : "https://www.youtube.com/embed/b-xuZIPQVZY"
+	}, {
+		"name" : {
+			"ENG" : "Mad Max, HotRod",
+			"RUS" : "Безумный Макс, HotRod, Ледышка"
+		},
+		"youtube" : "https://www.youtube.com/embed/je9PjmGymXo"
+	}, {
+		"name" : {
+			"ENG" : "Tanks",
+			"RUS" : "Танки и Белаз"
+		},
+		"youtube" : "https://www.youtube.com/embed/rAD6iJhyV5Q"
+	}, {
+		"name" : {
+			"ENG" : "The Ambulance!",
+			"RUS" : "Скорая помощь"
+		},
+		"youtube" : "https://www.youtube.com/embed/ve525n7Ygeo"
+	}, {
+		"name" : {
+			"ENG" : "Dinosaur",
+			"RUS" : "Динозавр"
+		},
+		"youtube" : "https://www.youtube.com/embed/HOug0te6G-k"
+	}
+]
