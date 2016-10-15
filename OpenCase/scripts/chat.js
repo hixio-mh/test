1181ded19711a2d186434caa9434771b43fa7836
VLADOS776 <kurtukovvlad@gmail.com>
2016-10-15 20:08:30 +0300

    chat Update

--- OpenCase/scripts/chat.js
+++ OpenCase/scripts/chat.js
@@ -1,11 +1,5 @@
-$(function() {
-    /*$("#chat__new-message").keyup(function(event) {
-        if (event.keyCode == 13) {
-            $("#chat__send-new-message").click();
-        }
-    });*/
-
-    $("#chat__new-message").on('keydown paste', function(event) {
+$(function () {
+    $("#chat__new-message").on('keydown paste', function (event) {
         if (event.keyCode == 13) {
             $("#chat__send-new-message").click();
             event.preventDefault();
@@ -14,109 +8,151 @@ $(function() {
             event.preventDefault();
         }
     });
-
-    firebase.auth().onAuthStateChanged(function(user) {
+    history.replaceState("chat-rooms", null, "chat.html")
+    var rooms = "";
+    for (var i = 0; i < fbChat.rooms.length; i++) {
+        rooms += "<div class='chat__rooms__room" + (Settings.language == fbChat.rooms[i].code ? ' playerLang' : '') + "' data-room=\"" + fbChat.rooms[i].code + "\">";
+        rooms += "<img src=\"../images/flags/" + fbChat.rooms[i].flag + "\"><span>" + fbChat.rooms[i].name + "</span></div>";
+    }
+    $('.chat__rooms').html(rooms);
+    firebase.auth().onAuthStateChanged(function (user) {
         // Once authenticated, instantiate Firechat with the logged in user
         if (firebase.auth().currentUser != null) {
             $("#login").hide();
-            $("#chat").show();
-        } else {
             $("#chat").hide();
+            $(".chat__rooms").show();
+        }
+        else {
+            $("#chat").hide();
+            $(".chat__rooms").hide();
             $("#login").show();
         }
-
     });
-
-    $(document).on('click', '#login button', function() {
+    $(document).on('click', '.chat__rooms__room', function () {
+        fbChat.setChatRef($(this).data('room'));
+        $("#login").hide();
+        $("#chat").show();
+        $(".chat__rooms").hide();
+        fbChat.initChat('.chat__messages');
+        history.pushState('chat-'+$(this).data('room'), "Chat Room", 'chat-'+$(this).data('room')+'.html')
+    })
+    $(document).on('click', '#login button', function () {
         $(".chat__messages").append('<li id="js-loading-inventory" data-from="1"><div class="cssload-container"><div class="cssload-speeding-wheel"></div></div></li>');
-        setTimeout(function() {
-            initChat();
+        setTimeout(function () {
+            fbChat.initChat('.chat__messages');
         }, 2000);
     });
-
-    $(document).on('click', '.chat__message img', function() {
+    $(document).on('click', '.chat__message img', function () {
         var uid = $(this).data('userid');
         if (typeof uid == 'undefined') return false;
         window.location = 'profile.html?uid=' + uid;
     })
 });
 
-
-var chatRef = firebase.database().ref('globalChat');
-
-chatRef.on('child_added', function(data) {
-    if ($("li[data-msgkey='" + data.key + "']").length == 0) {
-        newMsg(data.key, data.val().uid, data.val().img, data.val().username, data.val().time, data.val().text);
-        $("html, body").animate({
-            scrollTop: $(document).height()
-        }, 10);
+window.addEventListener('popstate', function(e) {
+    var prev = e.state;
+    if (prev == 'chat-rooms') {
+        $("#login").hide();
+        $("#chat").hide();
+        $(".chat__rooms").show();
+        history.pushState("chat-rooms", null, "chat.html");
+    } else if (/chat-\w{2}/.test(prev)) {
+        var room = prev.match(/chat-(\w{2})/)[1];
+        $(".chat__rooms__room[data-room='"+room+"']").click();
     }
 });
-
-chatRef.on('child_removed', function(data) {
-    removeMsg(data.key);
-});
-
-function sendChatMessage(userName, text, img) {
-    var time = new Date();
-    time = "" + time;
-    var uid = firebase.auth().currentUser.uid;
-    chatRef.push({
-        username: userName,
-        uid: uid,
-        text: text,
-        time: time,
-        img: img
-    });
-}
+var fbChat = (function (module) {
+    module.chatRef = firebase.database().ref('globalChat');
+    module.chatRoomsRef = firebase.database().ref('chatRooms');
+    module.rooms = [
+        {
+            name: "Русский"
+            , flag: 'RU.svg'
+            , code: 'RU'
+        , }, {
+            name: 'Polski'
+            , flag: 'PL.svg'
+            , code: 'PL'
+        , }, {
+            name: 'English'
+            , flag: 'EN.svg'
+            , code: 'EN'
+        , }, {
+            name: 'Deutsch'
+            , flag: 'DE.svg'
+            , code: 'DE'
+        , }, {
+            name: 'Français'
+            , flag: 'FR.svg'
+            , code: 'FR'
+        , }
+    ];
+    module.setChatRef = function(ref) {
+        module.chatRef = firebase.database().ref('chat/'+ref);
+    }
+    module.sendMsg = function (userName, text, img) {
+        var time = new Date();
+        time = "" + time;
+        var uid = firebase.auth().currentUser.uid;
+        fbChat.chatRef.push({
+            username: userName
+            , uid: uid
+            , text: text
+            , time: time
+            , img: img
+            , timestamp: firebase.database.ServerValue.TIMESTAMP
+        });
+    }
+    module.initChat = function (selector) {
+        $(selector + " li").remove();
+        fbChat.chatRef.on('child_added', function (data) {
+            if ($("li[data-msgkey='" + data.key + "']").length == 0) {
+                newMsg(data.key, data.val().uid, data.val().img, data.val().username, data.val().time, data.val().text);
+                $("html, body").animate({
+                    scrollTop: $(document).height()
+                }, 10);
+            }
+        });
+        fbChat.chatRef.on('child_removed', function (data) {
+            removeMsg(data.key);
+        });
+        /*return module.chatRef.once('value').then(function (snapshot) {
+            messages = snapshot.val();
+            for (key in messages) {
+                newMsg(key, messages[key].uid, messages[key].img, messages[key].username, messages[key].time, messages[key].text);
+            }
+        });*/
+    }
+    return module;
+}(fbChat || {}));
 
 function newMsg(key, uid, img, username, time, text) {
     var time = new Date(time);
     time = time.toLocaleString((Settings.language == 'RU') ? 'ru' : 'en-US', {
-        hour: 'numeric',
-        minute: 'numeric'
+        hour: 'numeric'
+        , minute: 'numeric'
     });
     var myMessage = false;
-
-    if (uid == firebase.auth().currentUser.uid)
-        myMessage = true;
-
+    if (uid == firebase.auth().currentUser.uid) myMessage = true;
     text = XSSreplace(text);
     username = XSSreplace(username);
-
-    var msg = "<li class='animated bounceIn chat__message" + (myMessage ? " my_message" : "") + "' data-msgkey='" + key + "'>" +
-        "<img src='" + img + "' data-userID='" + uid + "'>" +
-        "<div class='message__info'>" +
-        "<div class='message__info__from-time'>" +
-        "<span class='message__from'>" + username + "</span>" +
-        "<span class='message__time'>" + time + "</span>" +
-        "</div>" +
-        "<span class='message__text'>" + text + "</span>" +
-        "</div></li>";
+    var msg = "<li class='animated bounceIn chat__message" + (myMessage ? " my_message" : "") + "' data-msgkey='" + key + "'>" + "<img src='" + img + "' data-userID='" + uid + "'>" + "<div class='message__info'>" + "<div class='message__info__from-time'>" + "<span class='message__from'>" + username + "</span>" + "<span class='message__time'>" + time + "</span>" + "</div>" + "<span class='message__text'>" + text + "</span>" + "</div></li>";
     $(".chat__messages").append(msg);
 }
 
 function removeMsg(key) {
-    $("li[data-msgkey='" + key + "']").removeClass('bounceIn');
-    $("li[data-msgkey='" + key + "']").addClass('zoomOut');
-    setTimeout(function() {
-        $('.zoomOut').remove();
-    }, 1000);
+    try {
+        $("li[data-msgkey='" + key + "']").removeClass('bounceIn');
+        $("li[data-msgkey='" + key + "']").addClass('zoomOut');
+        setTimeout(function () {
+            $('.zoomOut').remove();
+        }, 1000);
+    }
+    catch (e) {}
 }
-
-function initChat() {
-    $(".chat__messages li").remove();
-    return chatRef.once('value').then(function(snapshot) {
-        messages = snapshot.val();
-        for (key in messages) {
-            newMsg(key, messages[key].uid, messages[key].img, messages[key].username, messages[key].time, messages[key].text);
-        }
-    });
-}
-
-$(document).on('click', '#chat__send-new-message', function() {
+$(document).on('click', '#chat__send-new-message', function () {
     var msg = $('#chat__new-message').text();
     if (msg.length == 0) return false;
-    sendChatMessage(Player.nickname, msg, '../images/ava/' + Player.avatar);
+    fbChat.sendMsg(Player.nickname, msg, '../images/ava/' + Player.avatar);
     $('#chat__new-message').empty();
 });
